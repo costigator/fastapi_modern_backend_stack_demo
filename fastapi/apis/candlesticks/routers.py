c284049fe065a26cf375873fcd982f4bfbf705ae
costigator <costigator@gmail.com>
2021-06-22 08:45:53 +0200

    limit in sql increased

--- fastapi/apis/candlesticks/routers.py
+++ fastapi/apis/candlesticks/routers.py
@@ -18,12 +18,12 @@ async def list_candlesticks(request: Request):
 @cache(expire=60)
 @router.get("/withrediscache", response_description="Same data but with Redis cache that expires after 60 seconds")
 async def list_candlesticks(request: Request):
-    result = request.app.clickhouse_client.execute("select * from data order by time desc limit 100")
+    result = request.app.clickhouse_client.execute("select * from data order by time desc limit 500")
 
     return result
 
 @router.get("/withratelimit", dependencies=[Depends(RateLimiter(times=1, seconds=5))], response_description="Same data but limited to 1 request every 5 seconds")
 async def list_candlesticks_limited(request: Request):
-    result = request.app.clickhouse_client.execute("select * from data order by time desc limit 100")
+    result = request.app.clickhouse_client.execute("select * from data order by time desc limit 500")
 
     return result
